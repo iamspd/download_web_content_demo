0a5fb06b3c9f7e69efc031b313301cb34c648635
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-07-29 17:52:26 -0400

    task implemented

--- app/src/main/java/com/example/downloadwebcontentdemo/MainActivity.java
+++ app/src/main/java/com/example/downloadwebcontentdemo/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +17,62 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            Log.i("Params: ", strings[0]);
+        protected String doInBackground(String... urls) {
 
-            return "Done";
+           // I will require the URL and HttpUrlConnection instances to
+           // read the string given in the param and do the
+           // operations
+
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+            String result = "";
+
+            // there could be instances where the url string is
+            // broken or an invalid url had passed
+            // so, the operation on the url string should be done
+            // in try, catch block
+
+            try{
+
+                // the url that is provided in the onCreate method via
+                // params passed in AsyncTask
+                url = new URL(urls[0]);
+
+                // now, to have the url data accessed
+                // create the connection, this is equivalent to opening a
+                // Web Browser.
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                // I will going to enter the url string into the browser window and
+                // let it establish the connection
+                InputStream inputStream = httpURLConnection.getInputStream();
+
+                // now, I will let the browser read this data contained in the url
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                // I will read the string
+                int data = reader.read();
+
+                // loop through the data one character at a time
+                while (data != -1) {
+
+                    // current character from the stream (url)
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+
+                return result;
+
+            } catch (Exception e){
+
+                e.printStackTrace();
+
+                return "Failed";
+            }
         }
     }
 
@@ -29,17 +85,13 @@ public class MainActivity extends AppCompatActivity {
         String result = null;
 
         try {
-            result = downloadTask.execute("https://www.google.com").get();
+            result = downloadTask.execute("https://www.google.com/").get();
 
-        } catch (ExecutionException e) {
+        } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
 
-        Log.i("Result: ", result);
-
-
+        Log.i("Contents: ", result);
     }
 }
