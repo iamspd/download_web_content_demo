4c0e25cbb7f5c997f43f6540976082c248ca58fb
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-07-29 16:37:09 -0400

    implemented AsyncTask

--- app/src/main/java/com/example/downloadwebcontentdemo/MainActivity.java
+++ app/src/main/java/com/example/downloadwebcontentdemo/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.downloadwebcontentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("Params: ", strings[0]);
+
+            return "Done";
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+        downloadTask.execute("https://www.google.com");
+
+
     }
 }
